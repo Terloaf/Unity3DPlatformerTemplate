1771e4528bdb4bf9a7ac0bcc70be8162cfb63e2f
Terloaf <greenyryan2501@gmail.com>
2026-02-08 15:33:04 -0400

    working on puzzle 4

---
+++ Assets/DoorOpen.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if())
+    }
+}
