d0533acfcb13384eee4bdfe7a34cdfe40a05cece
Terloaf <greenyryan2501@gmail.com>
2026-02-17 13:24:20 -0400

    Added weighted buttons that only need to be pressed once

---
+++ Assets/Scripts/WeightedButtonTrigger.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class WeightedButtonTrigger : MonoBehaviour
+{
+    public bool _isPressed = false;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _isPressed = true;
+
+            gameObject.GetComponent<Renderer>().materials[1].color = new Color(0f, 1f, 0f);
+            
+        }
+        if (collision.gameObject.CompareTag("Pickup"))
+        {
+            _isPressed = true;
+            gameObject.GetComponent<Renderer>().materials[1].color = new Color(0f, 1f, 0f);
+        }
+    }
+
+}
--- Assets/WeightedButtonSystem.cs
+++ Assets/WeightedButtonSystem.cs
@@ -1,61 +1,55 @@
-using JetBrains.Annotations;
-using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
-
-public class ButtonSystem : MonoBehaviour
+public class WeightedButtonSystem : MonoBehaviour
 {
 
     [SerializeField]
-    ButtonTriggerSystem[] _buttons;
-    
+    WeightedButtonTrigger[] _weightedButtons;
+
 
 
 
     [SerializeField]
     List<GameObject> triggerableGameObjects;
- 
-   
-    
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
 
-        
-       
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
 
-        for(int i = 0; i < _buttons.Length; i++)
+
+        for (int i = 0; i < _weightedButtons.Length; i++)
         {
-            if (_buttons[i]._isPressed == false)
+            if (_weightedButtons[i]._isPressed == false)
             {
                 return;
             }
         }
 
-        
+
         Debug.Log("all buttons pressed");
         for (int j = 0; j < triggerableGameObjects.Count; j++)
         {
-            
+
             ITriggerable triggerable = triggerableGameObjects[j].GetComponent<ITriggerable>();
             if (triggerable == null) return;
             triggerable.DoButtonLogic();
         }
-        
-        
-        
-        
-    }
-  
 
+
+
+
+    }
 
 }
